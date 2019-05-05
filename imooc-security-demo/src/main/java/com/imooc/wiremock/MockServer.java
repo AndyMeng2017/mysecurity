f6c6e206076ffddf71edcd140ba8cce7538d0bb6
menghaonan <menghaonan@sinovoice.com>
2019-05-05 22:33:45 +0800

    不使用ClassPathResource

--- imooc-security-demo/src/main/java/com/imooc/wiremock/MockServer.java
+++ imooc-security-demo/src/main/java/com/imooc/wiremock/MockServer.java
@@ -2,8 +2,8 @@ package com.imooc.wiremock;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MockServer {
@@ -11,17 +11,12 @@ public class MockServer {
         WireMock.configureFor(8082);
         WireMock.removeAllMappings();
 
-//        mock("/order/1", "1");
-
-        ClassPathResource resource = new ClassPathResource("D:\\IdeaProjects\\git\\mysecurity\\imooc-security-demo\\src\\main\\resources\\mock\\response\\1.txt");
-        String content = FileUtils.readFileToString(resource.getFile(), "UTF-8");
-        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/order/1"))
-                .willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
+        mock("/order/1", "1");
     }
 
     public static void mock(String url, String filename) throws IOException {
-        ClassPathResource resource = new ClassPathResource("D:\\IdeaProjects\\git\\mysecurity\\imooc-security-demo\\src\\main\\resources\\mock\\response\\" + filename + ".txt");
-        String content = FileUtils.readFileToString(resource.getFile(), "UTF-8");
+        File file = new File("D:\\IdeaProjects\\git\\mysecurity\\imooc-security-demo\\src\\main\\resources\\mock\\response\\" + filename + ".txt");
+        String content = FileUtils.readFileToString(file, "UTF-8");
         WireMock.stubFor(WireMock.get(WireMock.urlEqualTo(url))
                 .willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
     }
