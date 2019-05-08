860802fef07ce2b447f2cd388798a69bf201fc78
menghaonan <menghaonan1027@outlook.com>
2019-05-09 01:18:14 +0800

    添加banner.txt，自定义彩蛋

--- imooc-security-demo/src/main/java/com/imooc/code/DemoImageCodeGenerator.java
+++ imooc-security-demo/src/main/java/com/imooc/code/DemoImageCodeGenerator.java
@@ -1,10 +1,21 @@
 package com.imooc.code;
 
+import com.imooc.security.core.validate.code.ImageCode;
+import com.imooc.security.core.validate.code.ValidateCodeGenerator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
+
 /**
  * @Author: mhn
  * @Date: 2019/5/8 22:46
  * @Version 1.0
  * @Software: IntelliJ IDEA
  */
-public class DemoImageCodeGenerator {
+@Component(value = "imageCodeGenerator")
+public class DemoImageCodeGenerator implements ValidateCodeGenerator {
+    @Override
+    public ImageCode generate(ServletWebRequest request) {
+        System.out.println("更高级的图形验证码生成代码");
+        return null;
+    }
 }
